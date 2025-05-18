451e08a0b4ac413cc7a313efb8572fa57ce72c1c
Alvaro Carrera Montalvo <alvarocarreramontalvo@MacBook-Pro-de-Alvaro.local>
2025-05-17 21:24:02 -0500

    Primer Commit

---
+++ client/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@router': path.resolve(__dirname, './src/router'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@styles/variables";`, // Aplica globalmente
+      },
+    },
+  },
+});
